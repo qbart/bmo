1333872c8440e9076562903ad337b0cbd8ddffd7
bart <wojtowicz.bartlomiej@gmail.com>
2020-01-11 22:57:35 +0100

    test text example

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"fmt"
+	"image/color"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/text"
 )
 
@@ -12,7 +16,44 @@ const (
 	sh = 320
 )
 
+var (
+	sampleText      = `The quick brown fox jumps over the lazy dog.`
+	mplusNormalFont font.Face
+	mplusBigFont    font.Face
+)
+
+func init() {
+	tt, err := truetype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const dpi = 72
+	mplusNormalFont = truetype.NewFace(tt, &truetype.Options{
+		Size:    24,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	mplusBigFont = truetype.NewFace(tt, &truetype.Options{
+		Size:    48,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+}
+
 func update(screen *ebiten.Image) error {
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	// Draw info
+	msg := fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS())
+	text.Draw(screen, msg, mplusNormalFont, 20, 40, color.White)
+
+	// Draw the sample text
+	text.Draw(screen, sampleText, mplusNormalFont, 20, 80, color.White)
+
+
 	return nil
 }
 
